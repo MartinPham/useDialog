f440fd812edb6d27443acf2262a3f99df3e1d6f0
MartinPham <MartinPham@users.noreply.github.com>
2022-01-05 00:02:19 +0000

    Manual build dff5b49

--- .ci/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -
-. $(dirname "$0")/env.sh
-
-echo "[TEST] Testing"
-yarn test
-echo "[TEST] Done"
--- src/components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { App } from '.'
-
-test('renders App', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/ASK/i)
-  expect(linkElement).toBeInTheDocument()
-})
